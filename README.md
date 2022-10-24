da3392aae50e8afcac4f0917f27d659a46968094
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-10-24 05:27:21 -0400

    add new book modal ui

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# odin-library
+# Library
+
+A simple reading tracker web app.
---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+class Book {
+  constructor(title = String, author = String, read = Boolean) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary() {}
