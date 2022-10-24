2bbec94479b1c36af9c2ba9f1ad89b1fc533dab6
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-10-24 08:09:40 -0400

    complete crud functionality

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ const bookShelf = document.getElementById('bookShelf');
 const newBookModal = document.querySelector('.new-book-modal');
 
 const myLibrary = [];
+let lastId = 0;
 
 class Book {
-  constructor(title = String, author = String, read = Boolean, id = String) {
+  constructor(title = String, author = String, read = Boolean, id = Number) {
     this.title = title;
     this.author = author;
     this.read = read;
@@ -20,12 +21,16 @@ class Book {
 function buildBook(book) {
   const bookNode = document.createElement('div');
   bookNode.classList.add('book');
-  bookNode.id = book.id;
+  bookNode.id = `b${book.id}`;
   const titleNode = createSpan(['title']);
   const authorNode = createSpan(['author']);
   const readNode = createSpan(['read']);
   const removeBtn = document.createElement('button');
   removeBtn.textContent = 'Delete';
+  removeBtn.addEventListener('click', (e) => {
+    // let id = e.target.parentElement.id.substring(1) * 1;
+    removeBookFromLibrary(book.id);
+  });
 
   titleNode.textContent = book.title;
   authorNode.textContent = book.author;
@@ -36,13 +41,39 @@ function buildBook(book) {
   return bookNode;
 }
 
+function renderLibrary(booksNode, library) {
+  if (booksNode.childElementCount < library.length) {
+    library.map((book) => {
+      if (booksNode.querySelector(`#b${book.id}`) === null) {
+        let bookToInsert = buildBook(book);
+        booksNode.insertBefore(
+          bookToInsert,
+          booksNode.querySelector('#b' + book.id + 1)
+        );
+      }
+    });
+  } else if (booksNode.childElementCount > library.length) {
+    [...booksNode.childNodes].map((book) => {
+      let bookId = book.id.substring(1) * 1;
+      let idx = library.findIndex((bookEl) => bookEl.id === bookId);
+      if (idx < 0) {
+        book.remove();
+      }
+    });
+  }
+}
+
 function addBookToLibrary(title, author, read) {
-  let bookId = `b${myLibrary.length}`;
+  let bookId = lastId++;
   let book = new Book(title, author, read, bookId);
   myLibrary.push(book);
-  console.log(myLibrary);
-  let bookNode = buildBook(book);
-  bookShelf.appendChild(bookNode);
+  renderLibrary(bookShelf, myLibrary);
+}
+
+function removeBookFromLibrary(bookId) {
+  let idx = myLibrary.findIndex((book) => book.id === bookId);
+  myLibrary.splice(idx, 1);
+  renderLibrary(bookShelf, myLibrary);
 }
 
 function createSpan(classList) {
@@ -64,5 +95,3 @@ submitBook.addEventListener('click', (e) => {
     newBookModal.classList.add('hide');
   }
 });
-
-// Add a button on each book’s display to remove the book from the library.
