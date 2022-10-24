b378707046e1eb5048b4acae01f2b15456ce2105
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-10-24 07:16:26 -0400

    complete adding and rendering books functionality

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const read = document.getElementById('read');
-const addbookBtn = document.getElementById('addbook');
+const newBook = document.getElementById('newBook');
+const submitBook = document.getElementById('submitBook');
+const bookShelf = document.getElementById('bookShelf');
+const newBookModal = document.querySelector('.new-book-modal');
 
 const myLibrary = [];
 
@@ -17,11 +20,42 @@ function addBookToLibrary(title, author, read) {
   let book = new Book(title, author, read);
   myLibrary.push(book);
   console.log(myLibrary);
+  let bookNode = buildBook(title, author, read);
+  bookShelf.appendChild(bookNode);
 }
 
-addbookBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+function createSpan(classList) {
+  let span = document.createElement('span');
+  span.classList.add(...classList);
+  return span;
+}
+
+function buildBook(title, author, read) {
+  const book = document.createElement('div');
+  book.classList.add('book');
+  const titleNode = createSpan(['title']);
+  const authorNode = createSpan(['author']);
+  const readNode = createSpan(['read']);
+
+  titleNode.textContent = title;
+  authorNode.textContent = author;
+  readNode.textContent = read ? 'Have read' : "Haven't read";
+
+  book.append(titleNode, authorNode, readNode);
+
+  return book;
+}
+
+newBook.addEventListener('click', () => {
+  if (newBookModal.classList.contains('hide')) {
+    newBookModal.classList.remove('hide');
+  }
+});
+
+submitBook.addEventListener('click', (e) => {
   if (title.value && author.value) {
     addBookToLibrary(title.value, author.value, read.checked);
+    e.target.parentElement.reset();
+    newBookModal.classList.add('hide');
   }
 });
